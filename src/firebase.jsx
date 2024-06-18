c51ab4e4f3b9f31109bf708b0f5c9b8f09a1a8d2
Karan Kumar <114342084+Karannkx@users.noreply.github.com>
2024-06-18 16:33:38 +0530

    Update firebase.jsx

--- src/firebase.jsx
+++ src/firebase.jsx
@@ -1,32 +1,14 @@
-// import firebase from "firebase";
-
-// const firebaseConfig = {
-//     apiKey: "AIzaSyDORghRt7N_-1hkPMRD1741S29HiUPrrAE",
-//     authDomain: "clone-9cf5a.firebaseapp.com",
-//     projectId: "clone-9cf5a",
-//     storageBucket: "clone-9cf5a.appspot.com",
-//     messagingSenderId: "601612465450",
-//     appId: "1:601612465450:web:b66105f27337581aa3da53",
-//     measurementId: "G-4GWMCERJSB"
-//   };
-// const firebaseApp = firebase.initializeApp(firebaseConfig);
-
-// const db = firebaseApp.firestore();
-// const auth = firebase.auth();
-
-// export { db, auth };
-
 import firebase from "firebase/compat/app"
 import "firebase/compat/auth"
 import "firebase/compat/firestore"
 const firebaseConfig = {
-    apiKey: "AIzaSyDORghRt7N_-1hkPMRD1741S29HiUPrrAE",
-    authDomain: "clone-9cf5a.firebaseapp.com",
-    projectId: "clone-9cf5a",
-    storageBucket: "clone-9cf5a.appspot.com",
-    messagingSenderId: "601612465450",
-    appId: "1:601612465450:web:b66105f27337581aa3da53",
-    measurementId: "G-4GWMCERJSB"
+    apiKey: "MY API KEY OF FIREBASE PROJECT",
+    authDomain: "SOME DETAILS",
+    projectId: "SOME DETAILS",
+    storageBucket: "SOME DETAILS",
+    messagingSenderId: "SOME DETAILS",
+    appId: "SOME DETAILS",
+    measurementId: "SOME DETAILS"
   };
 //write these line
 const firebaseApp=firebase.initializeApp(firebaseConfig);
